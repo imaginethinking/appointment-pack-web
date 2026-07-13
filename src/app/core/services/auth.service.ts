d5027e2e4e23c2afd48a58349eac7215f2dc27b1
ImagineThinking <104801799+ElotherYT@users.noreply.github.com>
2026-07-13 21:05:12 +0100

    refactor(auth): update import statements

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import {inject, Injectable} from '@angular/core';
-import { Observable, tap, throwError} from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { Observable, tap, throwError } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
 
