a5c0ea25119d172d037ba1a5ed0ab58e4451c26f
ImagineThinking <104801799+ElotherYT@users.noreply.github.com>
2026-07-14 22:14:01 +0100

    feat(navbar): implement accessible navbar, works with desktop and mobile navigation

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  mobileMenuOpen = false;
+  profileMenuOpen = false;
+
+  toggleMobileMenu(): void {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+    this.profileMenuOpen = false;
+  }
+
+  toggleProfileMenu(): void {
+    this.profileMenuOpen = !this.profileMenuOpen;
+    this.mobileMenuOpen = false;
+  }
+
+  closeMenus(): void {
+    this.mobileMenuOpen = false;
+    this.profileMenuOpen = false;
+  }
+}
